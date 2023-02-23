25a2e1661d107a1d25aa3d996ea3f24821335f0c
AndreyMir <miroshal@mail.ru>
2023-02-23 03:34:36 +0300

    Задача 19

--- dz19/Program.cs
+++ dz19/Program.cs
@@ -3,24 +3,24 @@
 Console.Write("Введите пятизначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
+int a = N / 10000;
+int b = N % 10;
+    
+int c = N / 1000;
+int d = c % 10;
+
+int e = N / 10;
+int f = e % 10;
+
 if ((N < 9999) || (N > 100000))
 {
     Console.WriteLine("Неверная цифра. Введите пятизначное число. ");
 }
-    int a = N / 10000;
-    int b = N % 10;
-    
-    int c = N / 1000;
-    int d = c % 10;
-
-    int e = N / 10;
-    int f = e % 10;
-
-    if (a == b && d == f)
-    {
+else if (a == b && d == f)
+{
         Console.WriteLine($"число {N} является палиндромом");
-    }
-    else
-    {
-        Console.WriteLine($"число {N} не является палиндромом");
-    } 
+}
+else
+{
+    Console.WriteLine($"число {N} не является палиндромом");
+} 
